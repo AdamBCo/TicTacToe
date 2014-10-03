146b3e93392a849be8dea4e5e6c0b6978780d47e
Adam Cooper <acooper@rallydev.com>
2014-10-03 18:09:32 -0500

    Now Time to Clean up

--- TicTactoe/ViewController.m
+++ TicTactoe/ViewController.m
@@ -8,7 +8,7 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UIAlertViewDelegate>
 @property (strong, nonatomic) IBOutlet UILabel *labelOne;
 @property (strong, nonatomic) IBOutlet UILabel *labelTwo;
 @property (strong, nonatomic) IBOutlet UILabel *labelThree;
@@ -22,20 +22,38 @@
 @property CGPoint originalCenter;
 @property NSInteger playerNumber;
 @property NSMutableArray *myArray;
-@property NSInteger numberOfX;
 @property NSMutableArray *totalPointsArray;
+@property NSString *winner;
+@property UIAlertView *alertOne;
+@property UIAlertView *alertTwo;
 
 @end
+
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.numberOfX = 0;
-    self.labelOne.text = @"";
+    
     self.myArray = [NSMutableArray arrayWithObjects:self.labelOne, self.labelTwo, self.labelThree, self.labelFour, self.labelFive, self.labelSix, self.labelSeven, self.labelEight, self.labelNine, nil];
+    self.alertOne = [[UIAlertView alloc] initWithTitle:@"You Won!!" message:@"X is the Winner!!" delegate:self cancelButtonTitle:@"Cancel" otherButtonTitles:nil];
+    [self.alertOne addButtonWithTitle:@"New Game"];
+    self.alertTwo = [[UIAlertView alloc] initWithTitle:@"You Won!!" message:@"O is the Winner!!" delegate:self cancelButtonTitle:@"Cancel" otherButtonTitles:nil];
+    [self.alertTwo addButtonWithTitle:@"New Game"];
     self.totalPointsArray = [[NSMutableArray alloc] initWithCapacity:9];
     
+    for (int i = 0; i < self.myArray.count; i++) {
+        UILabel *labelMark = self.myArray[i];
+        labelMark.text = @"";
+    }
+    
+    
+}
+
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
+    if (buttonIndex == 1) {
+        [self newGame:self];
+    }
 }
 
 - (IBAction)onLabelTapped:(UITapGestureRecognizer *)tapGesture {
@@ -77,6 +95,7 @@
     }
     
     [self determineWinner];
+    [self findWinner];
     
 }
 
@@ -113,7 +132,6 @@
         if ([labelMark.text isEqualToString: @"X"]){
                 point = @"1";
                 [pointsArray insertObject:point atIndex:i];
-                self.numberOfX--;
             } else if ([labelMark.text isEqualToString:@"O"]){
                 point = @"-1";
                 [pointsArray insertObject:point atIndex:i];
@@ -124,6 +142,109 @@
     }
     self.totalPointsArray = pointsArray;
     NSLog(@"%@\n",self.totalPointsArray);
+}
+
+-(void) findWinner {
+    NSString *squareOne = self.totalPointsArray[0];
+    NSString *squareTwo = self.totalPointsArray[1];
+    NSString *squareThree = self.totalPointsArray[2];
+    NSString *squareFour = self.totalPointsArray[3];
+    NSString *squareFive = self.totalPointsArray[4];
+    NSString *squareSix = self.totalPointsArray[5];
+    NSString *squareSeven = self.totalPointsArray[6];
+    NSString *squareEight = self.totalPointsArray[7];
+    NSString *squareNine = self.totalPointsArray[8];
+    
+    NSInteger rowOne = squareOne.integerValue + squareTwo.integerValue + squareThree.integerValue;
+    NSInteger rowTwo = squareFour.integerValue + squareFive.integerValue + squareSix.integerValue;
+    NSInteger rowThree = squareSeven.integerValue + squareEight.integerValue + squareNine.integerValue;
+    
+    NSInteger columnOne = squareOne.integerValue + squareFour.integerValue + squareSeven.integerValue;
+    NSInteger columnTwo = squareTwo.integerValue + squareFive.integerValue + squareEight.integerValue;
+    NSInteger columnThree = squareThree.integerValue + squareSix.integerValue + squareNine.integerValue;
+    
+    NSInteger diaganolOne = squareOne.integerValue + squareFive.integerValue + squareNine.integerValue;
+    NSInteger diagonalTwo = squareThree.integerValue + squareFive.integerValue + squareSeven.integerValue;
+    
+    if (rowOne == -3) {
+        self.winner = @"O";
+        [self.alertTwo show];
+            NSLog(@"Hello");
+        }else if (rowOne == 3){
+            self.winner = @"X";
+            [self.alertOne show];
+    }
+    
+    if (rowTwo == -3) {
+        self.winner = @"O";
+        [self.alertTwo show];
+        NSLog(@"Hello");
+    }else if (rowTwo == 3){
+        self.winner = @"X";
+        [self.alertOne show];
+    }
+    
+    if (rowThree == -3) {
+        self.winner = @"O";
+        [self.alertTwo show];
+        NSLog(@"Hello");
+    }else if (rowThree == 3){
+        self.winner = @"X";
+        [self.alertOne show];
+    }
+    
+    
+    if (columnOne == -3) {
+        self.winner = @"O";
+        [self.alertTwo show];
+        NSLog(@"Hello");
+    }else if (columnOne== 3) {
+        self.winner = @"X";
+        [self.alertOne show];
+    }
+    if (columnTwo == -3) {
+        self.winner = @"O";
+        [self.alertTwo show];
+        NSLog(@"Hello");
+    }
+    else if (columnTwo == 3){
+        self.winner = @"X";
+        [self.alertOne show];
+    }
+    
+    
+    if (columnThree == -3) {
+        self.winner = @"O";
+        [self.alertTwo show];
+        NSLog(@"Hello");
+    }
+    else if (columnThree == 3){
+        self.winner = @"X";
+        [self.alertOne show];
+    }
+    if (diaganolOne == -3) {
+        self.winner = @"O";
+        [self.alertTwo show];
+        NSLog(@"Hello");
+    }
+    else if (diaganolOne ==3){
+        self.winner = @"X";
+        [self.alertOne show];
+        
+    }
+    if (diagonalTwo == -3) {
+        self.winner = @"O";
+        [self.alertTwo show];
+        NSLog(@"Hello");
+    }
+    else if (diagonalTwo == 3){
+        self.winner = @"X";
+        [self.alertOne show];
+    }
+    
+    NSLog(@"Row One: %ld", (long)rowOne);
+    
+    
 }
 
 
